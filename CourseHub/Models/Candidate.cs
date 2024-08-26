1b44301b9a8cea8312aea3ea5802decdea494e3e
Monkey <Monkey@DESKTOP-TMQHTSA>
2024-08-26 21:54:46 +0300

    Add project files.

---
+++ CourseHub/Models/Candidate.cs
@@ -0,0 +1,24 @@
+﻿namespace CourseHub.Models
+{
+    public class Candidate
+    {
+        public string? Email { get; set; } = String.Empty;
+
+        public string? FirstName { get; set; } = String.Empty;
+
+        public string? LastName { get; set; } = String.Empty;
+
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+       
+        public int? Age { get; set; }
+
+        public string? SelectedCourse { get; set; } = String.Empty;
+
+        public DateTime ApplyAt { get; set; }
+
+       public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
